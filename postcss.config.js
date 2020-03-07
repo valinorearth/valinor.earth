2d7bdac88433cb7e1c0e3fef4cc441277727e8a0
saneef <hello@saneef.com>
2020-03-08 00:44:05 +0530

    chore: adds postcss-purgecss in postcss pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.njk", "./src/**/*.html", "./src/**/*.md"],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
     require("cssnano"),
   ],
 };
