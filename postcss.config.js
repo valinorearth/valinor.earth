1fac26e30963504ccdb015fb7c75a6801b682be6
saneef <hello@saneef.com>
2020-03-14 23:32:56 +0530

    chore(postcss): add postcss nesting plugin

--- postcss.config.js
+++ postcss.config.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
+    require("postcss-nesting"),
     require("autoprefixer"),
     ...(process.env.NODE_ENV === "production"
       ? [purgecss, require("cssnano")]
