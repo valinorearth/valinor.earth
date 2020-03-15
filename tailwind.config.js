27be7a5222912fd412e74b8121d54c5a50d075d9
saneef <hello@saneef.com>
2020-03-15 22:04:41 +0530

    feat: render partner and customer links from data file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
       display: ["Noto Serif JP", "serif"],
       body: ["Noto Serif JP", "serif"],
     },
+    extend: {
+      spacing: {
+        "1/1": "100%",
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+      },
+    },
   },
   variants: {},
 };
