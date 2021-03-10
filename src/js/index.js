9dc31b0011ba23518452f0c82a5213668ea04b0f
saneef <hello@saneef.com>
2021-03-10 14:34:45 +0530

    fix(stats): prettify numbers in stats
    Removes animation from stats

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
 import Turbolinks from "turbolinks";
 import { Application } from "stimulus";
-import AnimatedNumber from "stimulus-animated-number";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 Turbolinks.start();
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
-application.register("animated-number", AnimatedNumber);
